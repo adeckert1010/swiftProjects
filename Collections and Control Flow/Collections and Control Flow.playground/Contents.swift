fc29add48d41c8b79c33e18001b8771e7373a953
Allen Deckert <adeckert1010@gmail.com>
2017-05-02 17:00:19 -0400

    First work on Collections and Control Flow

---
+++ Collections and Control Flow/Collections and Control Flow.playground/Contents.swift
@@ -0,0 +1,70 @@
+//Collections and Control Flow
+
+//Arrays
+
+var todo: [String] = ["Finish collections course", "Buy groceries", "Respond to emails"]
+
+//Add new item to end of array using append
+todo.append("Pick up dry cleaning")
+
+//Concatenating two arrays
+
+[1,2,3] + [4]
+
+//Concatenate array containing string literal to todo
+todo + ["Order book online"]
+//Performing the same operation using the unary addition operator
+todo += ["Order book online"]
+
+//Immutable Arrays
+let secondTaskList = ["Mow the lawn"]
+
+//secondTaskList.append("Pay bills") Error!
+//secondTaskList += ["Pay bills"] Error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
