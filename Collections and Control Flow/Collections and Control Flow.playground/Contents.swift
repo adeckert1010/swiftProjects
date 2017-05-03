9a1af777dcc3873560752c5cd2443a6352ca20b6
Allen Deckert <adeckert1010@gmail.com>
2017-05-02 22:45:57 -0400

    Intro to Dictionaries

--- Collections and Control Flow/Collections and Control Flow.playground/Contents.swift
+++ Collections and Control Flow/Collections and Control Flow.playground/Contents.swift
@@ -1,7 +1,6 @@
 //Collections and Control Flow
 
 //Arrays
-
 var todo: [String] = ["Finish collections course", "Buy groceries", "Respond to emails"]
 
 //Add new item to end of array using append
@@ -23,22 +22,40 @@ let secondTaskList = ["Mow the lawn"]
 //secondTaskList += ["Pay bills"] Error
 
 
+//Reading From Arrays
+let firstTask = todo[0]
+let thirdTask = todo[2]
+
+//Modifying Existing Values in an Array
+//(Mutating an array)
+todo[4] = "Brush teeth"
+todo[0] = "Watch Treehouse Video]"
+
+//Insert Using Indexes
+todo.insert("Watch TV", at: 2)
+
+//Removing Items From Arrays
+todo.remove(at: 2)
+
+todo.count
+//todo[5] CRASH! Do not use index value equal to count value
 
 
+var arrayOfInts: [Int]
 
+//Dictionaries
 
+/*
+        Airport Code (Key)      Airport Name (Value)
+        
+        LQA                     La Quardia
+        LHR                     Heathrow
+        CDG                     Charles de Gaulle
+        HKG                     Hong Kong International
+        DXB                     Dubai International
+ */
 
-
-
-
-
-
-
-
-
-
-
-
+let airportCodes: [String: String] = ["LGA": "La Guardia", "LHR": "Heathrow", "CDG": "Charles de Gaulle", "HKG": "Hong Kong International", "DXB": "Dubai International"]
 
 
 
